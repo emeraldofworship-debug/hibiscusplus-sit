ae06967a51f1824574f95ab25cfbe42471a60b25
emeraldofworship-debug <emeraldofworship@hibiscusplus.co.uk>
2026-04-29 01:26:57 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div style={{padding:40}}>
+      <h1>HibiscusPlus is Live 🌺</h1>
+      <p>Your premium tea & Zobo brand</p>
+    </div>
+  );
+}
